58c169abbbab5bb65bd6bb761ba6b971e96f90fa
MISTER_CHAN <mister.chan.tszto@gmail.com>
2026-02-22 12:57:43 +0800

    Add files via upload

--- qwerty_soengping.py
+++ qwerty_soengping.py
@@ -5,38 +5,40 @@ t.speed(0)
 t.ht()
 
 qwe = ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+hor_gap, ver_gap, hor_off, ver_off = 60, 90, -300, 0
 
 le_pos = {}
+t.color('red')
 for i in range(3):
-    nc = len(qwe[i])
-    w = nc / 10.0
-    for j in range(nc):
+    for j in range(len(qwe[i])):
         le = qwe[i][j]
-        x, y = float(j) / nc * 10 + w / 2, float(i)
-        le_pos[le] = (x, y)
+        x, y = (j + i * 0.5 / 3) * hor_gap, (2 - float(i)) * ver_gap
+        le_pos[le] = x, y
+        t.pu()
+        t.goto(x + hor_off, y + ver_off)
+        t.pd()
+        t.write(le, align='center')
 
 sy_list = ['aau', 'aai', 'aanaat', 'aamaap', 'au', 'ai', 'anat', 'amap', 'aangaakangak',
            'eoiui', 'eoneotonot', 'oengoek', 'yunyutunut', 'unguk', 'iu', 'init', 'imip', 'ingikengek', 'oi', 'ongok']
 
-sy_pos, col, row = {}, {}, {}
+sy_pos = {}
+sy_bl, sy_tr = (9 * hor_gap, 2 * ver_gap), (0, 0)
 for sy in sy_list:
     x, y = 0, 0
     for le in sy:
         x, y = x + le_pos[le][0], y + le_pos[le][1]
     x, y = x / len(sy), y / len(sy)
-    sy_pos[sy] = (x, y)
-    col[x], row[y] = 0, 0
+    sy_pos[sy] = x, y
+    sy_bl, sy_tr = (min(sy_bl[0], x), min(sy_bl[1], y)), (max(sy_tr[0], x), max(sy_tr[1], y))
 
-for i, k in enumerate(sorted(col)):
-    col[k] = i
-for i, k in enumerate(sorted(row)):
-    row[k] = i
-
+hor_ra, ver_ra = (9 * hor_gap) / (sy_tr[0] - sy_bl[0]), (2 * ver_gap) / (sy_tr[1] - sy_bl[1])
 ab = {'aanaat': 'aat', 'aamaap': 'aap', 'anat': 'at', 'amap': 'ap', 'aangaakangak': 'ak',
       'eoiui': 'ui', 'eoneotonot': 'ot', 'oengoek': 'oek', 'yunyutunut': 'ut', 'unguk': 'uk', 'init': 'it', 'imip': 'ip', 'ingikengek': 'ik', 'ongok': 'ok'}
+t.color('black')
 for sy in sy_list:
-    sp = sy_pos[sy]
+    x, y = sy_pos[sy]
     t.pu()
-    t.goto(col[sp[0]] * 30 - 300, (2 - row[sp[1]]) * 30)
+    t.goto((x - sy_bl[0]) * hor_ra + hor_off, (y - sy_bl[1]) * ver_ra + ver_off)
     t.pd()
     t.write(ab.get(sy, sy), align='center')
